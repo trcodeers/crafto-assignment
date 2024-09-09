605b7028b7c005f6d0be73aa4fb838ea9e0887bf
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-09-09 18:33:06 +0530

    route

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+import QuotesForm from "./Pages/QuotesForm";
+import Login from "./Pages/Login";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/product",
-    element: <Product/>,
+    element: <Login/>,
   }
 ]);
+
 function App() {
   return (
     <RouterProvider router={router} />
