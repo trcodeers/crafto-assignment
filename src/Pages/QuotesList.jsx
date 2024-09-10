38dac5807e3165be9286d83a05964e933f3b645b
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-09-11 01:46:07 +0530

    reusable card component

--- src/Pages/QuotesList.jsx
+++ src/Pages/QuotesList.jsx
@@ -4,6 +4,7 @@ import httpService from "../utility/httpService";
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import QuoteCard from '../component/QuoteCard';
 
 
 const QuoteListPage = () => {
@@ -60,26 +61,7 @@ const QuoteListPage = () => {
 
         <div className="flex flex-row flex-wrap justify-center">
           {quotes.map((quote, index) => (
-            <div key={index} className="relative bg-white shadow-md rounded-md overflow-hidden m-4 w-60 flex flex-col justify-between">
-              {quote.mediaUrl ? (
-                <div className="relative">
-                  <img src={quote.mediaUrl} alt="Quote" className="w-full h-48 object-cover" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    <p className="text-white text-lg font-semibold px-4 text-center">{quote.text}</p>
-                  </div>
-                </div>
-              ) : (
-                <div className="w-full h-48 bg-gray-300 flex items-center justify-center">
-                  <p className="text-black text-lg font-semibold px-4 text-center">{quote.text}</p>
-                </div>
-              )}
-
-              {/* Username and creation date - fixed at the bottom */}
-              <div className="p-4 mt-auto">
-                <p className="text-gray-700 font-medium">By {quote.username}</p>
-                <p className="text-gray-500 text-sm">Created on {new Date(quote.createdAt).toLocaleString()}</p>
-              </div>
-            </div>
+            <QuoteCard key={index} quote={quote} />
           ))}
         </div>
 
